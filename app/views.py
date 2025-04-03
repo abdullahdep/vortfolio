b2a107d5c284131c0b4f1ee28c73fac1291b2e9d
Abdullah <abdullah08dep@gmail.com>
2025-04-03 11:04:09 +0500

    Front Page of Portfolio done

--- app/views.py
+++ app/views.py
@@ -1,15 +1,21 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+siteName = "Vortfolio"
+
+siteName= {"siteName": siteName}
+
+
 def index(request):
-    return render(request, "index.html")
+    return render(request, "index.html" ,siteName)
 def about(request):
-    return render(request, "about.html")
+    return render(request, "about.html" ,  siteName)
 def projects(request):
-    return render(request, "projects.html")
+    return render(request, "projects.html", siteName)
 def services(request):
-    return render(request, "services.html")
+    return render(request, "services.html", siteName)
 def contact(request):
-    return render(request, "contact.html")
+    return render(request, "contact.html", siteName)
 def portfolio(request):
-    return render(request, "portfolio.html")
+    return render(request, "portfolio.html", siteName)
