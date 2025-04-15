91b1ad51290f76b1d647f1a7662a12d3b80806c0
Abdullah <abdullah08dep@gmail.com>
2025-04-15 11:45:29 +0500

    k

--- app/urls.py
+++ app/urls.py
@@ -1,5 +1,4 @@
 from django.urls import path
-from django.views.generic import TemplateView
 from . import views
 
 urlpatterns = [
@@ -10,8 +9,5 @@ urlpatterns = [
     path("contact", views.contact, name="contact"),
     path('portfolio', views.portfolio, name='portfolio'),
     path('learn', views.learn, name='learn'),
-    path('ads.txt', TemplateView.as_view(
-        template_name='ads.txt',
-        content_type='text/plain'
-    ), name='ads_txt'),
+    path('ads.txt', views.ads_txt, name='ads_txt'),
 ]
