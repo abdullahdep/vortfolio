e3d675a94214b19ddddc5cbf5a82aea315d34df1
abdullahdep <abdullah08dep@gmail.com>
2025-04-21 03:18:24 +0500

    a

--- app/urls.py
+++ app/urls.py
@@ -20,7 +20,7 @@ urlpatterns = [
     path('consultation/<int:id>/', views.consultation_detail, name='consultation_detail'),
     path('bca6356bcc6f4e32986944a2297de9e7.txt', views.serve_txt_file, name='serve_txt_file'),
     # path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='sitemap'),
-    path('sitemap.xml', views.dynamic_sitemap, name='dynamic_sitemap'),
+    path('sitemap_index.xml', views.dynamic_sitemap, name='dynamic_sitemap'),
     path('robots.txt', views.robots_txt, name='robots_txt'),
     path('html', views.html, name='html'),
 ]
